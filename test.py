56b2ef697bfe06a52dd511eb30247d14d7978b44
willilai <william.lai.23@bishops.com>
2024-07-05 17:36:09 +0200

    Initial test commands

--- test.py
+++ test.py
@@ -3,7 +3,12 @@ from dorna2 import Dorna
 def main():
     robot = Dorna()
     print(robot.connect("192.168.2.20"))
+    robot.set_motor(1)
 
+    robot.play(timeout=-1, cmd="jmove", rel=1, j0 = -10, vel = 20)
+    robot.play(timeout=-1, cmd="jmove", rel=1, j1 = 10, vel = 20)
+
+    robot.set_motor(0)
     robot.close()
 
 
