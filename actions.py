b57f1ec1ba13aa2ff98ae30c67193315ced32d61
willilai <william.lai.23@bishops.com>
2024-08-01 00:23:50 +0200

    integrated the Dorna Robotic Arm with the BioImage.io Chatbot

--- actions.py
+++ actions.py
@@ -1,5 +1,6 @@
 import asyncio
 from dorna2 import Dorna
+from time import sleep
 from pydantic import BaseModel, Field
 import json
 from imjoy_rpc import api
@@ -50,7 +51,7 @@ class RoboticArm:
         self.robot.jmove(
             rel=0,
             j5=0,
-            vel=80
+            vel=175
         )
         with open("positions.json", 'r') as positions_file: 
             self.positions = json.load(positions_file)
@@ -61,42 +62,50 @@ class RoboticArm:
 
     async def robot_info(self, funcname):
         print(f"\nTrack Command in {funcname}")
-        track_cmd = await self.run_sync(self.robot.track_cmd)
-        print(track_cmd)
+        # track_cmd = await self.run_sync(self.robot.track_cmd)
+        print(self.robot.track_cmd())
+        # print(track_cmd)
 
     async def pickup(self, pos, clawOpen, microscope):
         # lowers the claw
-        await self.run_sync(self.robot.lmove,
-            z = pos["z"]-59,
-            vel = 50
+        # await self.run_sync(self.robot.lmove,
+        self.robot.lmove(
+            z = pos["z"]-55,
+            vel = 50,
+            accel=500,
+            jerk=2000
         )
         # displays robot information
         await self.robot_info("pickup")
 
         if clawOpen:
-            await self.run_sync(self.robot.jmove,
+            # await self.run_sync(self.robot.jmove,
+            self.robot.jmove(
                 rel=0,
                 j5 = 0,
-                vel = 80
+                vel = 175
             )
         else: 
             # closes the claw
-            await self.run_sync(self.robot.jmove,
+            # await self.run_sync(self.robot.jmove,
+            self.robot.jmove(
                 rel=0,
                 j5 = -375,
-                vel = 80
+                vel = 175
             )
         # displays robot information
         await self.robot_info("pickup close claw")
         
         # lifts up the claw
         if microscope:
-            await self.run_sync(self.robot.lmove,
+            # await self.run_sync(self.robot.lmove,
+            self.robot.lmove(
                 z = pos["z"]-57+24, 
                 vel = 50
             )
         else:
-            await self.run_sync(self.robot.lmove,
+            # await self.run_sync(self.robot.lmove,
+            self.robot.lmove(
                 z = pos["z"], 
                 vel = 50
             )
@@ -113,7 +122,8 @@ class RoboticArm:
         Returns: None
         """
         # rotates the robot to the transportation position
-        await self.run_sync(self.robot.jmove,
+        # await self.run_sync(self.robot.jmove,
+        self.robot.jmove(
             timeout=10, 
             rel=0, 
             j0=0,
@@ -127,7 +137,8 @@ class RoboticArm:
         await self.robot_info("transport")
 
         # moves the sliding rail to the position given
-        await self.run_sync(self.robot.jmove,
+        # await self.run_sync(self.robot.jmove,
+        self.robot.jmove(
             rel=0,
             j6=j6,
             vel=120,
@@ -144,14 +155,17 @@ class RoboticArm:
         Returns: None
         """
         # moves the robotic arm to the initial starting position
-        await self.run_sync(self.robot.jmove,
+        # await self.run_sync(self.robot.jmove,
+        self.robot.jmove(
             timeout=10, 
             rel=0, 
             j1=100, 
             j2=-100, 
             j3=-88, 
             j4=0, 
-            vel=250
+            vel=250,
+            accel=500,
+            jerk=2000,
         )
         # displays robot information
         await self.robot_info("move_to_initial")
@@ -169,7 +183,8 @@ class RoboticArm:
         await self.transport(pos["j6"])
 
         # rotates the robotic arm first to avoid any obstables
-        await self.run_sync(self.robot.jmove,
+        # await self.run_sync(self.robot.jmove()
+        self.robot.jmove(
             rel=0,
             j0=pos["j0"],
             vel = 250
@@ -179,7 +194,8 @@ class RoboticArm:
         await self.robot_info("grab_from_holder")
         
         # moves the rest of the robot arm to the initial holder position
-        await self.run_sync(self.robot.jmove,
+        # await self.run_sync(self.robot.jmove,
+        self.robot.jmove(
                 rel=0,
                 x=pos["x"],
                 y=pos["y"],
@@ -209,17 +225,19 @@ class RoboticArm:
         await self.transport(pos["j6"])
 
         # rotates the robotic arm first to avoid any obstables
-        await self.run_sync(self.robot.jmove,
+        # await self.run_sync(self.robot.jmove()
+        self.robot.jmove(
             rel=0,
             j0=pos["j0"],
             vel = 250
         )
 
         # displays robot information
-        await self.robot_info("action_from_holder")
+        await self.robot_info("grab_from_holder")
         
         # moves the rest of the robot arm to the initial holder position
-        await self.run_sync(self.robot.jmove,
+        # await self.run_sync(self.robot.jmove,
+        self.robot.jmove(
                 rel=0,
                 x=pos["x"],
                 y=pos["y"],
@@ -230,7 +248,7 @@ class RoboticArm:
                 vel = 200
             )
         # displays robot information
-        await self.robot_info("action_from_holder")
+        await self.robot_info("grab_from_holder")
 
         await self.pickup(pos, True, False)
 
@@ -255,7 +273,8 @@ class RoboticArm:
         await self.transport(pos["j6"])
 
         # robots the robotic arm first to avoid any obstables
-        await self.run_sync(self.robot.jmove,
+        # await self.run_sync(self.robot.jmove,
+        self.robot.jmove(
             rel=0,
             j0=pos["j0"],
             vel = 200
@@ -270,7 +289,8 @@ class RoboticArm:
             deltaY = -90
 
         # moves the rest of the robotic arm to the initial microscope position
-        await self.run_sync(self.robot.jmove,
+        # await self.run_sync(self.robot.jmove,
+        self.robot.jmove(
             rel=0,
             x = pos["x"],
             y = pos["y"]-deltaY,
@@ -283,11 +303,12 @@ class RoboticArm:
         await self.robot_info("action_from_microscope")
 
         # moves the robotic arm into the microscope to be able to pick-up/place
-        await self.run_sync(self.robot.lmove,
+        # await self.run_sync(self.robot.lmove,
+        self.robot.lmove(
             rel=0,
             y = pos["y"],
             b = pos["b"],
-            vel = 50
+            vel = 20
         )
         # displays robot information
         await self.robot_info("action_from_microscope")
@@ -295,7 +316,8 @@ class RoboticArm:
         await self.pickup(pos, False, True)
 
         # moves the robotic arm out of the microscope
-        await self.run_sync(self.robot.lmove,
+        # await self.run_sync(self.robot.lmove,
+        self.robot.lmove(
             rel=0,
             y = pos["y"]-deltaY,
             z = pos["z"]+microOffset,
@@ -320,15 +342,14 @@ class RoboticArm:
         position = config.position
         pos = self.positions[position]
 
-        self.robot.connect("192.168.2.20")
-
         # this is to lower the claw compared to initial holder position as to not hit the light
         microOffset = -33
 
         await self.transport(pos["j6"])
 
         # robots the robotic arm first to avoid any obstables
-        await self.run_sync(self.robot.jmove,
+        # await self.run_sync(self.robot.jmove,
+        self.robot.jmove(
             rel=0,
             j0=pos["j0"],
             vel = 200
@@ -343,7 +364,8 @@ class RoboticArm:
             deltaY = -90
 
         # moves the rest of the robotic arm to the initial microscope position
-        await self.run_sync(self.robot.jmove,
+        # await self.run_sync(self.robot.jmove,
+        self.robot.jmove(
             rel=0,
             x = pos["x"],
             y = pos["y"]-deltaY,
@@ -356,11 +378,12 @@ class RoboticArm:
         await self.robot_info("action_from_microscope")
 
         # moves the robotic arm into the microscope to be able to pick-up/place
-        await self.run_sync(self.robot.lmove,
+        # await self.run_sync(self.robot.lmove,
+        self.robot.lmove(
             rel=0,
             y = pos["y"],
             b = pos["b"],
-            vel = 50
+            vel = 20
         )
         # displays robot information
         await self.robot_info("action_from_microscope")
@@ -368,7 +391,8 @@ class RoboticArm:
         await self.pickup(pos, True, True)
 
         # moves the robotic arm out of the microscope
-        await self.run_sync(self.robot.lmove,
+        # await self.run_sync(self.robot.lmove,
+        self.robot.lmove(
             rel=0,
             y = pos["y"]-deltaY,
             z = pos["z"]+microOffset,
@@ -379,8 +403,6 @@ class RoboticArm:
         await self.robot_info("action_from_microscope")
 
         await self.move_to_initial()
-
-        self.robot.close()
 
 def get_schema():
         return {
@@ -403,6 +425,7 @@ async def setup():
         "name": "Robotic Arm Control",
         "description": "moves the robotic arm to pickup and place objects from microscopes and plate holders",
         "get_schema": get_schema,
+        "run_in_executer": True,
         "tools": {
             "grab_from_holder": robot.grab_from_holder,
             "place_at_holder": robot.place_at_holder,
@@ -413,7 +436,7 @@ async def setup():
 
     server_url = "https://chat.bioimage.io"
     token = await login({"server_url": server_url})
-    server = await connect_to_server({"server_url": server_url, "token": token})
+    server = await connect_to_server({"server_url": server_url, "token": token, "method_timeout": 30})
     svc = await server.register_service(robotic_arm_control_extension)
 
     print(f"Extension service registered with id: {svc.id}, you can visit the service at:\n https://bioimage.io/chat?server={server_url}&extension={svc.id}&assistant=Skyler")
